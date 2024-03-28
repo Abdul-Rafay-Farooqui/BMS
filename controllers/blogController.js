dbc1ad8695ea5a2e4de3be92c0ca4a1b47866dcc
Abdul-Rafay-Farooqui <farooquiabdul85@gmail.com>
2024-03-28 17:58:30 +0500

    update the refreshing of the pages

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,4 +1,5 @@
-const Post = require('../models/postModel')
+const { ObjectId } = require('mongodb');
+const Post = require('../models/postModel');
 
 const loadBlog = async(req,res)=>{
     try {
@@ -26,11 +27,14 @@ const addComment = async(req,res)=>{
 
         var post_id = req.body.post_id;
         var username = req.body.username;
+        var email = req.body.email;
         var commnet = req.body.comment;
+
+        var comment_id = new ObjectId();
 
         await  Post.findByIdAndUpdate({_id:post_id},{
             $push:{
-                "comments":{ username:username, comment:commnet}
+                "comments":{ id:comment_id, username:username, email:email, comment:commnet}
             }
         });
 
@@ -41,10 +45,29 @@ const addComment = async(req,res)=>{
     }
 }
 
+// const doReply = async(req,res)=>{
+//     try {
+//         var reply_id = new ObjectId();
+//         console.log("api", req.body, reply_id)
+//         const updatedPost = await Post.updateOne({
+//             "_id": new ObjectId(req.body.post_id),
+//             "comment._id": new ObjectId(req.body.comment_id),
+//         },{
+//             $push:{
+//                 "comments.$.replies":{ _id:reply_id, name:req.body.name, reply:req.body.reply }
+//             }
+//         });
 
+//         res.status(200).send({success:true,msg:'Reply added'});
+
+//     } catch (error) {
+//         res.status(200).send({success:false,msg:error.message});
+//     }
+// }
 
 module.exports = {
     loadPost,
+    // doReply,
     loadBlog,
     addComment
 }
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/BMS");
 const express =  require("express");
 const app = express();
 
+var http = require('http').createServer(app);
+var { Server } = require('socket.io');
+var io = new Server(http,{});
+
+
 const isBlog = require("./middlewares/isBlog");
 
 app.use(isBlog.isBlog);
@@ -21,6 +26,19 @@ app.use('/',userRoute);
 const blogRoute = require("./routes/blogRoute");
 app.use('/',blogRoute);
 
-app.listen(3001,function(){
+io.on("connection",function(socket){
+    console.log('User Connected');
+
+    socket.on("new_post",function(formData){
+        console.log(formData);
+        socket.broadcast.emit("new_post",formData);
+
+    });
+});
+
+// app.listen(3001,function(){
+//     console.log("Server id running");
+// })
+http.listen(3001,function(){
     console.log("Server id running");
 })
